09a3ca61e42deb36005d9db528d1f70b44a0ecdc
Robert Adams <r0b4dams@gmail.com>
2024-02-06 22:18:35 -0800

    Add register route

--- src/blueprints/auth/__init__.py
+++ src/blueprints/auth/__init__.py
@@ -60,9 +60,42 @@ def login():
 #     return auth_controller.refresh()
 
 
-# @auth_blueprint.route("/register", methods=["GET"])
-# def register():
-#     return auth_controller.register()
+@auth_blueprint.route("/register", methods=["GET"])
+def register():
+    new_state = state.push_redirect_url(
+        request.args.get("redirect_uri"),
+        request.args.get("state")
+    )
+
+    code_verifier, code_challenge = generate_pkce_pair()
+
+    redirect_uri = "".join(
+        [request.scheme, "://", request.host, "/auth/callback"]
+    )
+
+    query = urlencode({
+        "response_type": "code",
+        "scope": "openid offline_access",
+        "client_id": request.args.get("client_id"),
+        "state": new_state,
+        "redirect_uri": redirect_uri,
+        "code_challenge": code_challenge,
+        "code_challenge_method": "S256"
+    })
+
+    redirect_url = "".join(
+        [FUSIONAUTH_BASE_URL, "/oauth2/register", "?", query])
+
+    response = redirect(redirect_url, code=302)
+
+    response.set_cookie(
+        key="code_verifier",
+        value=code_verifier,
+        secure=True,
+        httponly=True,
+        samesite="lax"
+    )
+    return response
 
 
 # @auth_blueprint.route("/user", methods=["GET"])
