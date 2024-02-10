e674a7bec92760bd932c34a57e18b4af51afa5f0
Robert Adams <r0b4dams@gmail.com>
2024-02-10 01:48:04 -0800

    Rm num workers fn

--- src/config.py
+++ src/config.py
@@ -3,21 +3,14 @@ import os
 mode = os.environ.get("MODE", "development")
 host = os.environ.get("HOST", "localhost")
 port = os.environ.get("PORT", "9000")
+workers = os.environ.get("NUM_WORKERS", '4')
 
 FUSIONAUTH_BASE_URL = os.environ.get(
-    "FUSIONAUTH_BASE_URL", "http://localhost:9011"
-)
+    "FUSIONAUTH_BASE_URL", "http://localhost:9011")
 FUSIONAUTH_CLIENT_ID = os.environ.get(
-    "FUSIONAUTH_CLIENT_ID", "6e4e9805-9690-476f-a7d8-2552992c41e1"
-)
+    "FUSIONAUTH_CLIENT_ID", "6e4e9805-9690-476f-a7d8-2552992c41e1")
 FUSIONAUTH_CLIENT_SECRET = os.environ.get(
-    "FUSIONAUTH_CLIENT_SECRET", "ZyYv1MrS4XjCZKMu0YShVXsGbXoHw57pkXNBcSukY48"
-)
-
-
-def number_of_workers():
-    # https://docs.gunicorn.org/en/latest/design.html#how-many-workers
-    return 4
+    "FUSIONAUTH_CLIENT_SECRET", "ZyYv1MrS4XjCZKMu0YShVXsGbXoHw57pkXNBcSukY48")
 
 
 config = {
@@ -25,5 +18,6 @@ config = {
     "host": host,
     "port": port,
     'bind': '%s:%s' % (host, port),
-    'workers': number_of_workers(),
+    # https://docs.gunicorn.org/en/latest/design.html#how-many-workers
+    'workers': workers,
 }
