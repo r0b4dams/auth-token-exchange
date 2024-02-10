42cf1ae19bedc556c71fff23839d6f4d72441e7a
Robert Adams <r0b4dams@gmail.com>
2024-02-10 01:47:15 -0800

    Add doc strs

--- src/server.py
+++ src/server.py
@@ -4,15 +4,22 @@ from gunicorn.app.base import BaseApplication
 
 
 class Server(BaseApplication):
-    def __init__(self, config: dict[str, str] = {}, blueprints: list[Blueprint] = []):
-        self.app = Flask(__name__)
-        CORS(self.app, supports_credentials=True)
+    """
+    Top-level class managing the application
+    """
+
+    def __init__(self, name: str, config: dict[str, str] = {}, blueprints: list[Blueprint] = []):
+        self.app = Flask(name)
         self.config = config
         self.register_blueprints(blueprints)
+        CORS(self.app, supports_credentials=True)
         super().__init__()
 
     def listen(self):
-
+        """
+        start the server and run forever
+        uses Gunicorn in production, else starts Flask dev server
+        """
         if self.config['mode'] == 'production':
             self.run()
         else:
@@ -23,19 +30,25 @@ class Server(BaseApplication):
             )
 
     def load(self):
+        """
+        used by BaseApplication to access the Flask instance
+        """
         return self.app
 
     def load_config(self):
-        # filter valid gunicorn properties
+        """
+        filter valid Gunicorn properties and set config on BaseApplication
+        """
         gunicorn_config = {
             key: value for key, value in self.config.items()
             if key in self.cfg.settings and value is not None
         }
-
-        # set config on BaseApplication
         for key, value in gunicorn_config.items():
             self.cfg.set(key.lower(), value)
 
     def register_blueprints(self, blueprints: list[Blueprint]):
+        """
+        add blueprints to the Flask app
+        """
         for blueprint in blueprints:
             self.app.register_blueprint(blueprint)
