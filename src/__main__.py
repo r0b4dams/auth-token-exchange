dbdc5b2512156d2870f7f9fda07ee3ded3c618ae
Robert Adams <r0b4dams@gmail.com>
2024-02-10 01:49:10 -0800

    Load project metadata to use in app

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __init__ import project_data
 from server import Server
 from config import config
 from blueprints import blueprints
 
 
-Server(config, blueprints).listen()
+Server(project_data["app_name"], config, blueprints).listen()
