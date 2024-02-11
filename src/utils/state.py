a5f76d46e94f4d640d37690c8b2c7ffb84a7d7ba
Robert Adams <r0b4dams@gmail.com>
2024-02-10 16:02:58 -0800

    Formatting

--- src/utils/state.py
+++ src/utils/state.py
@@ -6,11 +6,12 @@ from utils.b64 import encode, decode
 def generate_redirect_url(req: Request) -> str:
     encoded_uri, state, *_ = req.args.get("state").split(":")
     redirect_uri = decode(encoded_uri)
+
     query = urlencode({
         "state": state,
         "user_state": req.args.get("state"),
-        "locale": req.args.get("state")
-    })
+        "locale": req.args.get("state")})
+
     return "".join([redirect_uri, "?", query])
 
 
