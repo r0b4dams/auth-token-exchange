2247abfd5d14dbf69c6a279b2d41c42066115904
Robert Adams <r0b4dams@gmail.com>
2024-02-11 00:32:33 -0800

    Linting & Formatting

--- src/server.py
+++ src/server.py
@@ -1,3 +1,5 @@
+"""server"""
+
 from flask import Blueprint, Flask
 from flask_cors import CORS
 from gunicorn.app.base import BaseApplication
@@ -8,25 +10,25 @@ class Server(BaseApplication):
     Top-level class managing the application
     """
 
-    def __init__(self, name: str, config: dict[str, str] = {}, blueprints: list[Blueprint] = []):
+    def __init__(self, name: str, config: dict[str, str], blueprints: list[Blueprint]):
         self.app = Flask(name)
         self.config = config
         self.register_blueprints(blueprints)
         CORS(self.app, supports_credentials=True)
         super().__init__()
 
+    def init(self, *_):
+        pass
+
     def listen(self):
         """
         start the server and run forever
         uses Gunicorn in production, else starts Flask dev server
         """
-        if self.config['mode'] == 'production':
+        if self.config["mode"] == "production":
             self.run()
         else:
-            self.app.run(
-                debug=True,
-                host=self.config['host'],
-                port=self.config['port'])
+            self.app.run(debug=True, host=self.config["host"], port=self.config["port"])
 
     def load(self):
         """
@@ -38,8 +40,11 @@ class Server(BaseApplication):
         """
         filter valid Gunicorn properties and set config on BaseApplication
         """
-        gunicorn_config = {key.lower(): value for key, value in self.config.items()
-                           if key in self.cfg.settings and value is not None}
+        gunicorn_config = {
+            key.lower(): value
+            for key, value in self.config.items()
+            if key in self.cfg.settings and value is not None
+        }
 
         for key, value in gunicorn_config.items():
             self.cfg.set(key, value)
