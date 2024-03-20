768a69b4c1f6b5e37280d1915b0d67401e45f64c
Robert Adams <r0b4dams@gmail.com>
2024-03-20 13:02:54 -0700

    Update app structure

--- src/blueprints/__init__.py
+++
@@ -1,3 +0,0 @@
-from blueprints.auth import auth_blueprint
-
-blueprints = [auth_blueprint]
--- src/blueprints/auth/controllers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""blueprints.auth.controllers"""
-
-from .callback import *
-from .login import *
-from .logout import *
-from .refresh import *
-from .register import *
-from .user import *
