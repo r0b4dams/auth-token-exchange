d7cd302beb663fc6e886e9cce9a412adcb9de933
Robert Adams <r0b4dams@gmail.com>
2024-04-20 08:32:46 -0700

    Update import

--- src/texserv/utils/b64.py
+++ src/texserv/utils/b64.py
@@ -1,15 +1,15 @@
-from base64 import b64encode, b64decode
+import base64
 
 ENCODING = "ascii"
 
 
 def encode(string: str) -> str:
     str_bytes = string.encode(ENCODING)
-    b64_bytes = b64encode(str_bytes)
+    b64_bytes = base64.b64encode(str_bytes)
     return b64_bytes.decode(ENCODING)
 
 
 def decode(string: str) -> str:
     b64_bytes = string.encode(ENCODING)
-    str_bytes = b64decode(b64_bytes)
+    str_bytes = base64.b64decode(b64_bytes)
     return str_bytes.decode(ENCODING)
