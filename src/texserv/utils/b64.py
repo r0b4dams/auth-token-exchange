c77793d73e792c53f343f77e5cfddac6c33e4520
Robert Adams <r0b4dams@gmail.com>
2024-04-19 21:51:51 -0700

    Rm doc strs

--- src/texserv/utils/b64.py
+++ src/texserv/utils/b64.py
@@ -1,25 +1,15 @@
-"""
-TODO: doc str
-"""
-
 from base64 import b64encode, b64decode
 
 ENCODING = "ascii"
 
 
 def encode(string: str) -> str:
-    """
-    TODO: doc str
-    """
     str_bytes = string.encode(ENCODING)
     b64_bytes = b64encode(str_bytes)
     return b64_bytes.decode(ENCODING)
 
 
 def decode(string: str) -> str:
-    """
-    TODO: doc str
-    """
     b64_bytes = string.encode(ENCODING)
     str_bytes = b64decode(b64_bytes)
     return str_bytes.decode(ENCODING)
