a880dd0e8966d742158b03b8f0049790fdeb9722
Robert Adams <r0b4dams@gmail.com>
2024-04-19 20:48:27 -0700

    Rm module doc str

--- src/texserv/blueprints/auth/controllers/__init__.py
+++ src/texserv/blueprints/auth/controllers/__init__.py
@@ -1,9 +1,5 @@
-"""
-TODO: doc str
-"""
-
 from .callback import handle_callback
-from .login import handle_login
+from .signup_login import handle_signup_login
 from .logout import handle_logout
 from .refresh import handle_refresh
 from .register import handle_register
