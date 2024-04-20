71ca99de43dfc30908eaade2cbd3fff80772dee0
Robert Adams <r0b4dams@gmail.com>
2024-04-20 08:32:58 -0700

    Rm deleted module import

--- src/texserv/blueprints/auth/controllers/__init__.py
+++ src/texserv/blueprints/auth/controllers/__init__.py
@@ -2,5 +2,4 @@ from .callback import handle_callback
 from .signup_login import handle_signup_login
 from .logout import handle_logout
 from .refresh import handle_refresh
-from .register import handle_register
 from .user import handle_user
