46d7b42f3f44fcb76b31994470b2c28429847df7
Robert Adams <r0b4dams@gmail.com>
2024-04-28 13:57:31 -0700

    Bump version to test

--- src/authexchange/__init__.py
+++ src/authexchange/__init__.py
@@ -4,5 +4,5 @@
 """
 A server to handle the OAuth token exchange with FusionAuth.
 """
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __app_name__ = __name__  # for use in __main__ to avoid override
