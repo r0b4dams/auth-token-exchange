1ac8f6c149099dff2029f29f3903a1a28bae8017
Robert Adams <r0b4dams@gmail.com>
2024-04-28 12:01:13 -0700

    Add docker push to release workflow

--- src/authexchange/__init__.py
+++ src/authexchange/__init__.py
@@ -4,5 +4,5 @@
 """
 A server to handle the OAuth token exchange with FusionAuth.
 """
-__version__ = "0.7.3"
+__version__ = "0.7.4"
 __app_name__ = __name__  # for use in __main__ to avoid override
