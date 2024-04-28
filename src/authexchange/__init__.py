f70638b16898f56b391f7b9b26c936afbc7cb745
Robert Adams <r0b4dams@gmail.com>
2024-04-28 14:56:29 -0700

    Remove name, restore version

--- src/authexchange/__init__.py
+++ src/authexchange/__init__.py
@@ -4,5 +4,4 @@
 """
 A server to handle the OAuth token exchange with FusionAuth.
 """
-__version__ = "0.0.6"
-__app_name__ = __name__  # for use in __main__ to avoid override
+__version__ = "0.7.5"
